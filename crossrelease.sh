7b6011d372148b02003f7a59de77316099641341
Erik van oosten <e.vanoosten@grons.nl>
2014-07-09 20:15:05 +0200

    Akka 2.3.4 was released, closes #48

--- crossrelease.sh
+++ crossrelease.sh
@@ -8,4 +8,4 @@ CMD=${1:-$DEFAULT_COMMAND}
 sbt '; set akkaVersion := "";      set crossScalaVersions := Seq("2.10.0", "2.11.0")' +$CMD \
     '; set akkaVersion := "2.1.4"; set crossScalaVersions := Seq("2.10.0")'           +$CMD \
     '; set akkaVersion := "2.2.4"; set crossScalaVersions := Seq("2.10.0")'           +$CMD \
-    '; set akkaVersion := "2.3.3"; set crossScalaVersions := Seq("2.10.0", "2.11.0")' +$CMD
+    '; set akkaVersion := "2.3.4"; set crossScalaVersions := Seq("2.10.0", "2.11.0")' +$CMD
