2dfbea00f6dea87936fdc49a2ea2cda6555f97bd
Maciej Bilas <maciej.bilas@gmail.com>
2015-08-28 15:51:33 +0300

    Implicits: test implicitness of funToMetricFilter.

--- src/test/scala/nl/grons/metrics/scala/ImplicitsSpec.scala
+++ src/test/scala/nl/grons/metrics/scala/ImplicitsSpec.scala
@@ -7,9 +7,10 @@ import org.scalatest.{FunSpec, Matchers}
 class ImplicitsSpec extends FunSpec with Matchers {
 
   describe("Implicits.funToMetricFilter") {
-    it("convert a (String, Metric) => Boolean function to a MetricFilter of the same semantics") {
-      val emptyMetric = new Metric {}
+
+    it("converts a (String, Metric) => Boolean function to a MetricFilter of the same semantics") {
       val fun = (name: String, _: Metric) => !name.contains("foo")
+      val emptyMetric = new Metric {}
       val filter = new MetricFilter {
         override def matches(name: String, metric: Metric) = !name.contains("foo")
       }
@@ -21,6 +22,15 @@ class ImplicitsSpec extends FunSpec with Matchers {
         matchResult shouldEqual expectedResult
         filter.matches(name, emptyMetric) shouldEqual filterFromFun.matches(name, emptyMetric)
       }
+    }
+  }
+
+  describe("Implicits") {
+    it("should bring a implicit conversions from (String, Metric) => Boolean to MetricFilter into scope") {
+      import Implicits._
+      def filterFun(name: String, metric: Metric) = false
+      val filter: MetricFilter = filterFun _
+      filter shouldBe a[MetricFilter]
     }
   }
 
