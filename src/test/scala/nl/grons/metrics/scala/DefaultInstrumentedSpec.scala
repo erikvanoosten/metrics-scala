5196c09ed674b4573e865b08dd513772f7b408e0
Jeff Klukas <jeff@klukas.net>
2016-09-02 13:11:07 -0400

    Add DefaultInstrumented trait

---
+++ src/test/scala/nl/grons/metrics/scala/DefaultInstrumentedSpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2016 Erik van Oosten
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.grons.metrics.scala
+
+import com.codahale.metrics.SharedMetricRegistries
+import org.junit.runner.RunWith
+import org.scalatest.{BeforeAndAfter, FunSpec}
+import org.scalatest.Matchers._
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class DefaultInstrumentedSpec extends FunSpec with BeforeAndAfter {
+
+  before {
+    SharedMetricRegistries.clear()
+  }
+
+  describe("DefaultInstrumented") {
+    it("throws if no default registry exists on construction") {
+      intercept[IllegalStateException] {
+        new DefaultInstrumented {}
+      }
+    }
+    it("has the expected metricRegistry") {
+      val defaultRegistry = SharedMetricRegistries.getOrCreate("default")
+      val instrumented = new DefaultInstrumented {}
+      instrumented.metricRegistry should be theSameInstanceAs defaultRegistry
+    }
+  }
+
+}
