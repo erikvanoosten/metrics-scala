80332ccb32cba3de3a31206cd27310c9d5ff12a0
Erik van Oosten <e.vanoosten@grons.nl>
2018-01-01 12:12:50 +0100

    Its 2018.

--- src/test/scala/nl/grons/metrics/scala/DefaultInstrumentedSpec.scala
+++ src/test/scala/nl/grons/metrics/scala/DefaultInstrumentedSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017 Erik van Oosten
+ * Copyright (c) 2016-2018 Erik van Oosten
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
