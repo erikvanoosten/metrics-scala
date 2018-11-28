c83762044613751bb1de8cb7da436b50c0ca2b0d
Erik van Oosten <e.vanoosten@grons.nl>
2018-11-28 12:18:17 +0100

    Sbt 0.13.15 -> 1.2.6.

--- project/build.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-import Keys._
-
-object MetricsScalaBuild extends Build {
-  lazy val akkaVersion = settingKey[String]("Version of Akka compiled against")
-
-  akkaVersion := "2.3.0"
-}
