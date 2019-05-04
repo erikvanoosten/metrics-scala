bd044bd5cb7b3e54fc38464a774a98c959919c08
Erik van Oosten <e.vanoosten@grons.nl>
2019-05-04 12:00:40 +0200

    Its 2019

--- metrics-scala/src/test/scala/nl/grons/metrics4/scala/InstrumentedBuilderSpec.scala
+++ metrics-scala/src/test/scala/nl/grons/metrics4/scala/InstrumentedBuilderSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2018 Erik van Oosten
+ * Copyright (c) 2014-2019 Erik van Oosten
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
